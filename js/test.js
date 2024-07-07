fc7f78c8a5ca22cade82d45ed01d7603fba109b1
domdavid <domdavid@outlook.com>
2024-07-07 13:01:09 -0700

    wavesurfer

---
+++ js/test.js
@@ -0,0 +1,19 @@
+//Creating  a wavesurfer instance, passing the container selector along with some options like progress color and wave color
+var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'violet',
+    progressColor: 'purple'
+  });
+  
+  //Loading the audio file you want to play
+  wavesurfer.load('audio/LOVE_s.mp3');
+  
+  // You can also load wav files if you want
+  //wavesurfer.load('audio/songaudio.wav');
+  
+  //This is the event you play the wavesurver instance i.e when wavesurfer ready event
+  wavesurfer.on('ready', function() {
+    wavesurfer.play();
+  });
+  
+  
