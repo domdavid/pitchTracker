4120d669c4f99aea88be70bdb314bbd933936228
domdavid <domdavid@outlook.com>
2024-07-08 12:18:45 -0700

    import

--- js/test.js
+++ js/test.js
@@ -1,3 +1,6 @@
+import WaveSurfer from 'wavesurfer.js'
+
+
 //Creating  a wavesurfer instance, passing the container selector along with some options like progress color and wave color
 const wavesurfer = WaveSurfer.create({
     container: '#waveform',
